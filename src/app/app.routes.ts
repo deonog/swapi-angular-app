e3813c345b24a368033e11d2d8b9dabc66f4919a
deonog <deon.ogbuokiri@gmail.com>
2026-02-23 21:25:28 +0100

    feat: add navigation and routing
    - Configure routes for all pages
    - Add header navigation component
    - Update app template with header and router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
   { path: 'films', loadComponent: () => import('./features/films/films.component').then(m => m.FilmsComponent) },
+  { path: 'films/detail', loadComponent: () => import('./features/films/film-detail.component').then(m => m.FilmDetailComponent) },
   { path: 'characters', loadComponent: () => import('./features/characters/characters.component').then(m => m.CharactersComponent) },
+  { path: 'characters/detail', loadComponent: () => import('./features/characters/character-detail.component').then(m => m.CharacterDetailComponent) },
   { path: 'planets', loadComponent: () => import('./features/planets/planets.component').then(m => m.PlanetsComponent) },
+  { path: 'planets/detail', loadComponent: () => import('./features/planets/planet-detail.component').then(m => m.PlanetDetailComponent) },
 ];
